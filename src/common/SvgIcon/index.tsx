96c93ef40289f3f242d3b91ea9fcdd34486fe2aa
Bratet <ahmedmrabet.002@gmail.com>
2023-11-08 01:31:06 +0100

    fixed deployement bug with svg

--- src/common/SvgIcon/index.tsx
+++ src/common/SvgIcon/index.tsx
@@ -1,5 +1,6 @@
 import { SvgIconProps } from "../types";
 
 export const SvgIcon = ({ src, width, height }: SvgIconProps) => (
-  <img src={`/img/svg/${src}`} alt={src} width={width} height={height} />
+  <img src={`${process.env.PUBLIC_URL}/img/svg/${src}`} alt={src} width={width} height={height} />
 );
+
--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import Router from "./router";
 import i18n from "./translation";
 
 const App = () => (
-  <BrowserRouter>
+  <BrowserRouter basename="/website">
     <I18nextProvider i18n={i18n}>
       <Router />
     </I18nextProvider>
